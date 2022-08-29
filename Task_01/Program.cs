a1118f997c43b07fa8b1b69ca43352dfd4e0991c
«MikleKho» <khomich.mikhail@gmail.com>
2022-08-29 17:20:39 +0300

    Решение Task_01

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -1,1 +1,49 @@
 ﻿// Задача 1: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+double[,] Mass_rnd_inject(double[,] massInside)
+{
+    Random rnd = new Random();
+
+    for (int i = 0; i < massInside.GetLength(0); i++)
+    {
+        for (int j = 0; j < massInside.GetLength(1); j++)
+        {
+            massInside[i, j] = Math.Round((rnd.NextDouble() * 10), 2);
+        }
+    }
+
+    return massInside;
+}
+
+void MassPrint(double[,] massInside)
+{
+    System.Console.WriteLine("Массив -->  ");
+
+    for (int i = 0; i < massInside.GetLength(0); i++)
+    {
+        for (int j = 0; j < massInside.GetLength(1); j++)
+        {
+            System.Console.Write($"  {massInside[i, j].ToString("F2")}");
+        }
+        System.Console.WriteLine();
+    }
+
+    return;
+}
+
+int massNumberRows = Prompt("Введите количество строк массива (>= 1) -->  ");
+int massNumberColumns = Prompt("Введите количество столбцов массива (>= 1) -->  ");
+
+double[,] mass = new double[massNumberRows, massNumberColumns];
+
+mass = Mass_rnd_inject(mass);
+MassPrint(mass);
+
